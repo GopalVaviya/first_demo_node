14514ed3b88a7afc098c78d7c68135f2fd848bf9
gopal vaviya <gopalvaviya123@gmail.com>
2024-12-19 12:54:00 +0530

    Landing page post route email submit

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.get_landing = (req, res) => {
     return res.render('landing', { title: 'Index page'})
 }
+
+exports.submit_lead = (req, res) => {
+    console.log("lead email:", req.body.lead_email)
+    return res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const landing = require('../controllers/landing')
 const routes = express.Router()
 
 routes.get('/', landing.get_landing)
+routes.post('/', landing.submit_lead)
 
 module.exports = routes
